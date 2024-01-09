c2d734fa0a4ce1dc8cb58846f3cdd52c9d5a250d
Julia Martins <martinss.s.julia@gmail.com>
2024-01-08 19:23:53 -0500

    feat: added header, footer and main routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,9 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import { log } from 'console';
+import mongoose  from 'mongoose';
 
+mongoose.connect(process.env.MONGODB_CONNECTIONG_STRING as string);
 
 const app = express();
 app.use(express.json()); 
