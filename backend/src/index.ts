0972952073c780efeeaddef03d4da52603e81f4f
Julia Martins <martinss.s.julia@gmail.com>
2024-01-11 12:32:58 -0500

    feat: added user login api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose  from 'mongoose';
 import userRoutes from './routes/users';
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTIONG_STRING as string);
 
@@ -12,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
 
 app.listen(7000, ()=> {
     console.log("server running on localhost 7000");
