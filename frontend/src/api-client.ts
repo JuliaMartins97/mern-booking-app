dfcb605a95254e36effd4a0751c2e0e8d0f59272
Julia Martins <martinss.s.julia@gmail.com>
2024-01-15 21:54:02 -0500

    feat: checking user login

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -2,9 +2,10 @@ import { RegisterFormData } from "./pages/Register";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const register =async (formData:RegisterFormData) => {
+export const register = async (formData:RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
-        method: 'POST',
+        method: "POST",
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
         },
@@ -16,4 +17,15 @@ export const register =async (formData:RegisterFormData) => {
     if(!response.ok) {
         throw new Error(responseBody.message)
     }
-}
+};
+
+export const validateToken = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+        credentials: "include"
+    })
+
+    if(!response.ok) {
+        throw new Error("Token invalid");
+    }
+    return response.json();
+};
