02219f55c49bfac2f29cc52eae6a7908dbf1364c
Julia Martins <martinss.s.julia@gmail.com>
2024-01-18 21:47:55 -0500

    fix: fixing build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,9 @@ app.use(
 }));
 
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
